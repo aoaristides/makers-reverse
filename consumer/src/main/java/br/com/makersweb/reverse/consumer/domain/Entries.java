7ea1b7865b4d7c3b08311d1ad73d62c7f4a6ea35
Anderson O. Aristides <joyce.miranda@marisa.com.br>
2023-09-03 21:57:09 -0300

    feat: Cofiguração Inicial

---
+++ consumer/src/main/java/br/com/makersweb/reverse/consumer/domain/Entries.java
@@ -0,0 +1,4 @@
+package br.com.makersweb.reverse.consumer.domain;
+
+public class Entries {
+}
---
+++ consumer/src/main/java/br/com/makersweb/reverse/consumer/domain/OrderType.java
@@ -0,0 +1,11 @@
+package br.com.makersweb.reverse.consumer.domain;
+
+/**
+ * @author aaristides
+ */
+public enum OrderType {
+
+    REVERSE,
+    REFUND
+
+}
