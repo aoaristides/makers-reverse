cc1dda9c7e7fd7da0e6ae19eaf62f27418282711
Anderson O. Aristides <joyce.miranda@marisa.com.br>
2023-09-13 18:22:30 -0300

    Ajuste no template terraform sns

--- infra/terraform/sns.tf
+++ infra/terraform/sns.tf
@@ -16,6 +16,20 @@ resource "aws_sns_topic_subscription" "reversaPorCartaoCreditoQueue_subscription
   endpoint             = aws_sqs_queue.reversaPorCartaoCreditoQueue.arn
 }
 
+resource "aws_sns_topic_subscription" "reversaPorCartaoDebitoQueue_subscription" {
+  protocol             = "sqs"
+  raw_message_delivery = true
+  topic_arn            = aws_sns_topic.reversaRecebidaNotification.arn
+  endpoint             = aws_sqs_queue.reversaPorCartaoDebitoQueue.arn
+}
+
+resource "aws_sns_topic_subscription" "reversaPorPixQueue_subscription" {
+  protocol             = "sqs"
+  raw_message_delivery = true
+  topic_arn            = aws_sns_topic.reversaRecebidaNotification.arn
+  endpoint             = aws_sqs_queue.reversaPorPixQueue.arn
+}
+
 resource "aws_sqs_queue_policy" "reversaPorBoletoQueue_subscription" {
   queue_url = aws_sqs_queue.reversaPorBoletoQueue.id
   policy    = <<EOF
@@ -72,3 +86,58 @@ resource "aws_sqs_queue_policy" "reversaPorCartaoCreditoQueue_subscription" {
 EOF
 }
 
+resource "aws_sqs_queue_policy" "reversaPorCartaoDebitoQueue_subscription" {
+  queue_url = aws_sqs_queue.reversaPorCartaoDebitoQueue.id
+  policy    = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "sns.amazonaws.com"
+      },
+      "Action": [
+        "sqs:SendMessage"
+      ],
+      "Resource": [
+        "${aws_sqs_queue.reversaPorCartaoDebitoQueue.arn}"
+      ],
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sns_topic.reversaRecebidaNotification.arn}"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_sqs_queue_policy" "reversaPorPixQueue_subscription" {
+  queue_url = aws_sqs_queue.reversaPorPixQueue.id
+  policy    = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "sns.amazonaws.com"
+      },
+      "Action": [
+        "sqs:SendMessage"
+      ],
+      "Resource": [
+        "${aws_sqs_queue.reversaPorPixQueue.arn}"
+      ],
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sns_topic.reversaRecebidaNotification.arn}"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
