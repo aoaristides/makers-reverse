de57784130b510c44b2ae21d38246ca0b9563560
Anderson O. Aristides <joyce.miranda@marisa.com.br>
2023-09-13 16:34:36 -0300

    Criado fila de receber reversa via terraform

--- infra/terraform/sqs.tf
+++ infra/terraform/sqs.tf
@@ -143,18 +143,18 @@
 #   policy    = "${data.aws_iam_policy_document.sqs_queue_policy_document.json}"
 # }
 
-resource "aws_sqs_queue" "first_queue" {
-  name                       = "hibicode-first-queue"
+resource "aws_sqs_queue" "recebeReversaDoPedidoQueue" {
+  name                       = "recebeReversaDoPedidoQueue"
   visibility_timeout_seconds = 30
   max_message_size           = 262144
   message_retention_seconds  = 1209600
   receive_wait_time_seconds  = 5
-  redrive_policy             = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.first_queue_dlq.arn}\",\"maxReceiveCount\":5}"
+  redrive_policy             = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.recebeReversaDoPedidoQueue_dlq.arn}\",\"maxReceiveCount\":5}"
   delay_seconds              = 0
 }
 
-resource "aws_sqs_queue" "first_queue_dlq" {
-  name                       = "hibicode-first-queue-dlq"
+resource "aws_sqs_queue" "recebeReversaDoPedidoQueue_dlq" {
+  name                       = "recebeReversaDoPedidoQueue-dlq"
   visibility_timeout_seconds = 30
   max_message_size           = 262144
   message_retention_seconds  = 1209600
@@ -164,7 +164,7 @@ resource "aws_sqs_queue" "first_queue_dlq" {
 
 data "aws_iam_policy_document" "sqs_queue_policy_document" {
   statement {
-    sid = "HibicodeSQSQueuesPolicy"
+    sid = "MakersWebSQSQueuesPolicy"
 
     principals {
       type = "AWS"
@@ -185,12 +185,12 @@ data "aws_iam_policy_document" "sqs_queue_policy_document" {
   }
 }
 
-resource "aws_sqs_queue_policy" "first_queue_policy" {
-  queue_url = "${aws_sqs_queue.first_queue.id}"
+resource "aws_sqs_queue_policy" "recebeReversaDoPedidoQueue_policy" {
+  queue_url = "${aws_sqs_queue.recebeReversaDoPedidoQueue.id}"
   policy    = "${data.aws_iam_policy_document.sqs_queue_policy_document.json}"
 }
 
-resource "aws_sqs_queue_policy" "first_queue_dlq_policy" {
-  queue_url = "${aws_sqs_queue.first_queue_dlq.id}"
+resource "aws_sqs_queue_policy" "recebeReversaDoPedidoQueue_dlq_policy" {
+  queue_url = "${aws_sqs_queue.recebeReversaDoPedidoQueue_dlq.id}"
   policy    = "${data.aws_iam_policy_document.sqs_queue_policy_document.json}"
 }
