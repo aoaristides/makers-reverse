fc3f0d9d90473ac18b69e69be1f56bcdcfcb6ea2
Anderson O. Aristides <joyce.miranda@marisa.com.br>
2023-09-13 16:47:34 -0300

    Criado fila de receber reversa por boleto via terraform

--- infra/terraform/sqs.tf
+++ infra/terraform/sqs.tf
@@ -161,6 +161,24 @@ resource "aws_sqs_queue" "recebeReversaDoPedidoQueue_dlq" {
   receive_wait_time_seconds  = 0
 }
 
+resource "aws_sqs_queue" "reversaPorBoletoQueue" {
+  name                       = "reversaPorBoletoQueue"
+  visibility_timeout_seconds = 30
+  max_message_size           = 262144
+  message_retention_seconds  = 1209600
+  receive_wait_time_seconds  = 5
+  redrive_policy             = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.reversaPorBoletoQueue_dlq.arn}\",\"maxReceiveCount\":5}"
+  delay_seconds              = 0
+}
+
+resource "aws_sqs_queue" "reversaPorBoletoQueue_dlq" {
+  name                       = "reversaPorBoletoQueue-dlq"
+  visibility_timeout_seconds = 30
+  max_message_size           = 262144
+  message_retention_seconds  = 1209600
+  receive_wait_time_seconds  = 0
+}
+
 
 data "aws_iam_policy_document" "sqs_queue_policy_document" {
   statement {
@@ -192,5 +210,15 @@ resource "aws_sqs_queue_policy" "recebeReversaDoPedidoQueue_policy" {
 
 resource "aws_sqs_queue_policy" "recebeReversaDoPedidoQueue_dlq_policy" {
   queue_url = "${aws_sqs_queue.recebeReversaDoPedidoQueue_dlq.id}"
+  policy    = "${data.aws_iam_policy_document.sqs_queue_policy_document.json}"
+}
+
+resource "aws_sqs_queue_policy" "reversaPorBoletoQueue_policy" {
+  queue_url = "${aws_sqs_queue.reversaPorBoletoQueue.id}"
+  policy    = "${data.aws_iam_policy_document.sqs_queue_policy_document.json}"
+}
+
+resource "aws_sqs_queue_policy" "reversaPorBoletoQueue_dlq_policy" {
+  queue_url = "${aws_sqs_queue.reversaPorBoletoQueue_dlq.id}"
   policy    = "${data.aws_iam_policy_document.sqs_queue_policy_document.json}"
 }
